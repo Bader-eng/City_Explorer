89df6a19baf864cc97566991ed87942884439da4
Bader-eng <baderali2016eng@gmail.com>
2021-04-27 22:13:22 +0300

    done

--- server.js
+++ server.js
@@ -1,100 +1,65 @@
 'use strict';
 const express = require('express');
 require('dotenv').config();
-const cors = require('cors');
-const superagent = require('superagent');
 
+const cors = require('cors');
 
 const server=express();
+
 const PORT=process.env.PORT || 3000;
-
-
 
 server.listen(PORT,()=>{
   console.log(`listening on port ${PORT}`);
 });
+
 server.use(cors());
 
+server.get('/data',(req,res)=>{
+  res.status(200).send('Hi from the data page, I am the server !!!');
+});
 
-// server.get('/data',(req,res)=>{
-//   res.status(200).send('Hi from the data page, I am the server !!!');
-// });
+server.get('/',(req,res)=>{
+  res.send('Hi this in my page!');
+});
 
-//	http://localhost:5000/location?city=amman
+server.get('/location',(req,res)=>{
+  let locationData=require('./data/location.json');
+  //console.log(locationData);
+  let locationRes= new Location(locationData);
+  res.send(locationRes);
+});
 
-server.get('/location',locationHandler);
-server.get('/weather',weatherHandler);
-server.get('*',generalHandler);
+let weather =[];
+server.get('/weather',(req,res)=>{
+  let weatherData=require('./data/weather.json').data;
+  weatherData.forEach(item =>{
+    let weatherRes= new Weather (item);
 
-
-
-
-
-function locationHandler(req,res){
-  let cityName=req.query.city;
-  let key=process.env.LOCATION_KEY;
-  //pk.f7c16ea4ef7fffdc3b4cbaeb3fd07102
-  let locURL=` https://us1.locationiq.com/v1/search.php?key=${key}&q=${cityName}&format=json`;
-  superagent.get(locURL).then(geodata=>{
-    let gData = geoData.body;
-    let locationData = new Location(cityName,gData);
-    res.send(locationData);
-   })
-   .catch(error=>{
-    console.log(error);
-    res.send(error);
-})
-  
-}
-
-
-
-
-function weatherHandler(req,res){
-  let weather =[];
-let cityName=req.query.city
-let key=process.env.WEATHER_KEY;
-let weaURL=`http://api.weatherbit.io/v2.0/forecast/daily?city=${cityName}&key=${key}`
-superagent.get(weaURL).then(wetDATA=>{
-  wetDATA.body.data.map(item=>{
-  let weatherRes= new Weather (item);
- weather.push(weatherRes);
+    weather.push(weatherRes);
   });
   res.send(weather);
- })
- .catch(error=>{
-  console.log(error);
-  res.send(error);
-})
-  // let weatherData=require('./data/weather.json').data;
-  // let weather =[];
-  // weatherData.forEach(item =>{
-  //   let weatherRes= new Weather (item);
-  //   weather.push(weatherRes);
-  // });
-}
 
-
-
+});
 
 function Weather(local){
   this.forecast=local.weather.description;
-  this.time=local.datetime;
+  this.time= new Date(local.datetime).toString().slice(0,15);
+
 }
 
-function Location(cityName,locData){
-  this.search_query=cityName;
+function Location(locData){
+  this.search_query='Lynnwood';
   this.formatted_query=locData[0].display_name;
   this.latitude=locData[0].lat;
   this.longitude=locData[0].lon;
 }
 
-
-function generalHandler(req,res){
+server.get('/*',(req,res)=>{
   let errObj = {
     status: 500,
     resText: 'sorry! this page not found'
+    
   };
   res.status(404).send(errObj);
-}
-const 
+});
+//http://localhost:5000/weather
